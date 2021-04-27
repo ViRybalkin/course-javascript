6be8fb68ce2f7e42bb4ecf7e601356393770e6cd
Vi <kinjeks@gmail.com>
2021-04-27 13:42:35 +0300

    initil

--- projects/array-objects/index.js
+++ projects/array-objects/index.js
@@ -9,7 +9,13 @@
  Пример:
    forEach([1, 2, 3], (el) => console.log(el))
  */
-function forEach(array, fn) {}
+
+function forEach(array, fn) {
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    fn(element, i, array);
+  }
+}
 
 /*
  Задание 2:
@@ -20,7 +26,23 @@ function forEach(array, fn) {}
  Пример:
    map([1, 2, 3], (el) => el ** 2) // [1, 4, 9]
  */
-function map(array, fn) {}
+// function map(array, fn) {
+//   const result = []
+//   for (let i = 0; i < array.length; i++) {
+//   result.push(fn(array[i],i,array));
+//   }
+//   return result
+//   }
+
+function map(array, fn) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    const newValue = fn(element, i, array);
+    newArray.push(newValue);
+  }
+  return newArray;
+}
 
 /*
  Задание 3:
@@ -31,8 +53,14 @@ function map(array, fn) {}
  Пример:
    reduce([1, 2, 3], (all, current) => all + current) // 6
  */
-function reduce(array, fn, initial) {}
-
+function reduce(array, fn, initial) {
+  let result = initial;
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    result = fn(result, element, i, array);
+  }
+  return result;
+}
 /*
  Задание 4:
 
@@ -41,13 +69,20 @@ function reduce(array, fn, initial) {}
  Пример:
    upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
  */
-function upperProps(obj) {}
+function upperProps(obj) {
+  const newObj = [];
+  for (const key in obj) {
+    const newValue = key.toUpperCase();
+    newObj.push(newValue);
+  }
+  return newObj;
+}
 
 /*
  Задание 5 *:
 
  Функция принимает объект и должна вернуть Proxy для этого объекта
- Proxy должен перехватывать все попытки записи значений свойств и возводить это значение в квадрат
+ Proxy должен перехватывать все попытки записи значений свойств и возводить это значение в к1вадрат
 
  Пример:
    const obj = createProxy({});
